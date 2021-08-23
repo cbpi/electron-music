4996d053e4c8adef5673b388a5433fa9cf8658a5
陈波 <chenbo900918@163.com>
2021-08-23 16:45:11 +0800

    配置eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+const { NoEmitOnErrorsPlugin } = require('webpack')
+
 module.exports = {
   root: true,
   parser: 'babel-eslint',
@@ -12,14 +14,22 @@ module.exports = {
   globals: {
     __static: true
   },
-  plugins: [
-    'html'
-  ],
-  'rules': {
+  plugins: ['html'],
+  rules: {
+    semi: ['error', 'never', { beforeStatementContinuationChars: 'never' }],
+    'semi-spacing': ['error', { after: true, before: false }],
+    'semi-style': ['error', 'first'],
+    'no-extra-semi': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-unreachable': 'error',
+    'space-before-blocks': 'off',
+    quotes: 'off',
+    'no-extra-semi': 'off',
     'global-require': 0,
     'import/no-unresolved': 0,
     'no-param-reassign': 0,
     'no-shadow': 0,
+    'comma-dangle': ['error', 'never'],
     'import/extensions': 0,
     'import/newline-after-import': 0,
     'no-multi-assign': 0,
